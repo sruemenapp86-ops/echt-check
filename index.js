9e7e1d772f780ccd7807c3e955f3ace0fb81fbd1
Stefan R <s.ruemenapp86@gmail.com>
2026-03-29 11:39:41 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const app = express();
 const upload = multer({ storage: multer.memoryStorage(), limits: { fileSize: 20 * 1024 * 1024 } });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 
 const db = new Pool({
   host: process.env.DB_HOST || 'postgres',
